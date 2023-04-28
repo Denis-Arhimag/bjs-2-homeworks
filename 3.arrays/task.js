5f95ec57e9673b0067b7dd2ee01bc57e02fe9830
Denis <lobovdenis.olegovich@gmail.com>
2023-04-28 15:56:42 +0300

    Домашнее задание номер 3, полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.toString() === arr2.toString();
+    // const compareArray = (a, b) => a.length === b.length && a.every((n, i) => n === b[i]);
+    // return compareArray; 
+    
+    // Но почему то этот вариант у меня не сработал. Что именно я не так написал ? 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length === 0) {
+        return 0;
+      } else if (gender != "мужской" && gender != "женский") {
+        return 0;
+      } else {
+        const adults = users.filter (person => person.gender === gender);
+        const amount = adults.reduce ((total, person) => total + person.age, 0);
+        return amount / adults.length
+      }
 }
